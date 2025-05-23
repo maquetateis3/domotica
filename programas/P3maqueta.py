cb32f14d8bb2d472cbc380d76231eca66593daab
marta-333 <martacernadasns3@gmail.com>
2025-05-23 17:02:38 +0200

    Update P3maqueta.py

--- programas/P3maqueta.py
+++ programas/P3maqueta.py
@@ -3,6 +3,6 @@ from microbit import *
 while True:
     if button_a.is_pressed():
         pin14.write_digital(1)
-        sleep(5000)
         music.play(music.RINGTONE)
-        sleep
+        sleep(5000)
+        pin14.write_digital(0)
